a607d7bad8f0bf321455d10a2526335bb0c64382
Golsa Taghavi <golsaroza@icloud.com>
2022-08-31 20:11:37 -0400

    eddited a portion of the js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function writePassword() {
 function generatePassword() {
   var passwordLenght = parseInt(prompt("Choose a number lenght between 8-128 characters"))
   if (passwordLenght < 8 || passwordLenght > 128 || isNaN(passwordLenght)) {
-    return 'please enter a number between'
+    return 'please choose an answer within the choosen criteria'
   }
   //create the prompts
   var upperCase = confirm("Would you like the passwrod to have uppercase letters?")
