eb477734ceeceb22529bc97b75bde13deb19d128
Golsa Taghavi <golsaroza@icloud.com>
2022-08-31 20:08:39 -0400

    comments were added to the js code section

--- script.js
+++ script.js
@@ -12,26 +12,30 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// create the lenght of the password and return when the choosen criteria is not selected
 function generatePassword() {
   var passwordLenght = parseInt(prompt("Choose a number lenght between 8-128 characters"))
   if (passwordLenght < 8 || passwordLenght > 128 || isNaN(passwordLenght)) {
     return 'please enter a number between'
   }
+  //create the prompts
   var upperCase = confirm("Would you like the passwrod to have uppercase letters?")
   var lowerCase = confirm("Would you like the passwrod to have lowercase letters?")
   var numbers = confirm("Would you like the passwrod to have numbers letters?")
   var specialCharacter = confirm("Would you like the passwrod to have specialCharacter letters?")
 
+  //what heppens if the given criteria is not selected?
   if (!upperCase && !lowerCase && !numbers && !specialCharacter) {
     return 'please choose an answer within the choosen criteria'
   }
-
+ // define each prompts
   var notHack = []
   if (upperCase) notHack += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   if (lowerCase) notHack += "abcdefghijklmnopqrstuvwxyz"
   if (numbers) notHack += "1234567890"
   if (specialCharacter) notHack += "!@#$%^&*"
   var password = ''
+    //the final password calculated after all of the prompts were answered
   for (i = 0; i < passwordLenght; i++) {
     var indexU = Math.floor(Math.random() * notHack.length)
     password += notHack[indexU]
